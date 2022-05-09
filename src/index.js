cc8289113395c5a2d8f8d36b250967075dcd7d03
Nemanja Nedeljkovic <nemanjan00@gmail.com>
2022-05-09 12:57:54 +0200

    API route

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const app = require("./app");
 const config = require("./config");
 
+app.use("/api", require("./api"));
+
 app.listen(config.get("PORT"), () => {
 	console.log(`[🚀]  Running on the ${config.get("PORT")} port.`);
 });
